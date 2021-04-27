fd4e144f0d216c3753ff087c26f532a320fd4b26
Cesar Vaz <cesar_augusto_perfil@hotmail.com>
2021-04-26 22:34:31 -0300

    adicionando promise

--- api/src/controllers/StudentController.js
+++ api/src/controllers/StudentController.js
@@ -9,63 +9,61 @@ module.exports = {
   },
 
   async create(request, response, next) {
-    try {
-      const { file } = request;
-      
-      const stream = fs.createReadStream(file.path);
-
-      const parseFile = csvParse({
-        delimiter: ";"
-      });
-
-      stream.pipe(parseFile);
-
-      parseFile.on("data", async (line) => {
-        const [ 
+    return new Promise((resolve, reject) => {
+      try {
+        const { file } = request;
+        
+        const stream = fs.createReadStream(file.path);
+        const parseFile = csvParse({ delimliter: ";" });
+  
+        stream.pipe(parseFile);
+  
+        parseFile
+          .on("data", async (line) => {
+            const [ 
+              name,
+              registration, 
+              login, 
+              cpf, 
+              id_group, //serviço
+              subject, // só para teste
+              status,
+              entry_time, 
+              supervisor, 
+              coordinator,
+              workload,
+              status_training
+            ] = line;
+            console.log(name);
+          })
+          .on("end", () => {
+            fs.promises.unlink(file.path);
+            resolve();
+          })
+          .on("error", (err) => {
+            reject(err);
+          });
+  
+        const { 
           name,
           registration, 
           login, 
           cpf, 
           id_group, //serviço
-          subject, // só para teste
-          status,
           entry_time, 
           supervisor, 
-          coordinator,
-          workload,
-          status_training
-        ] = line;
-        console.log(name);
-      });
-
-      const { 
-        name,
-        registration, 
-        login, 
-        cpf, 
-        id_group, //serviço
-        entry_time, 
-        supervisor, 
-        coordinator, 
-        status 
-      } = request.body;
-
-      await knex('students').insert({
-        name,
-        registration, 
-        login, 
-        cpf, 
-        id_group, 
-        entry_time, 
-        supervisor, 
-        coordinator, 
-        status
-      });
-
-      return response.status(201).send();
-    } catch (error) {
-      next(error);
-    }
+          coordinator, 
+          status 
+        } = request.body;
+  
+        await knex('students').insert({});
+  
+        return response.status(201).send();
+      } catch (error) {
+        next(error);
+      }
+    });
+    
   },
 
   async update(request, response, next) {
